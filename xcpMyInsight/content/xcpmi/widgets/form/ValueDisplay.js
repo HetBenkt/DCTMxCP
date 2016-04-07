0bd5116d2ea66858d65299bdc571c64231841f64
HetBenkt <theboss@home.nl>
2016-04-07 10:16:42 +0200

    Final for a POC version

--- xcpMyInsight/content/xcpmi/widgets/form/ValueDisplay.js
+++ xcpMyInsight/content/xcpmi/widgets/form/ValueDisplay.js
@@ -52,7 +52,7 @@ Ext.define("xcpmi.widgets.form.ValueDisplay", {
     		else
     			id = this.alias[0];
     		
-    		console.log(id + ':', arguments);
+    		console.log(id, arguments);
     	}
     },    
     
@@ -65,83 +65,95 @@ Ext.define("xcpmi.widgets.form.ValueDisplay", {
     },
     
 	setValue: function(value) {
-		//Javascript in MyInsight report
-    	//parent.Ext.getCmp(tagId).fireEvent('mi_event', 'test');
-		
-		//get id from widget
-		var tagId = this.id;
-		console.log(tagId);
-		
-		//get prefix myInsight URL
-		//http://{host_name}:{port}/eDRG
-		var urlValue = this.url;
-		console.log(urlValue);
-		
-		//Rewrite URL with correct paramaters; they should come from a server call
-		//http://{host_name}:{port}/eDRG?clsValue=MI&user=dmadmin&repository=MY_REPO&ticket=admin
-		
-		/*
-		//Does not support async calls!
-		Ext.Ajax.request({
-			async: false,
-			url: 'http://172.25.180.131:8081/MyInsight/currentuser',
-			callback: function(options, success, response) {
-				console.log(JSON.parse(response.responseText).properties.login_name);
-			}
-		});
-		*/
-		
-		var xhr = new XMLHttpRequest();
-		var dataUser = undefined;
-		var dataRepo = undefined;
-		var flag = 0;
-		var urlCurrentUser = 'http://172.25.180.131:8081/MyInsight/currentuser';
-		var urlRepositoryName = 'http://172.25.180.131:8081/MyInsight/repositories/xcp_repository';
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4 && xhr.status == 200) {
-				if(flag == 0) {
-					dataUser = JSON.parse(xhr.responseText).properties.login_name;
-					console.log(dataUser);
-				}
-				else {
-					dataRepo = JSON.parse(xhr.responseText).properties.name;
-					console.log(dataRepo);
-				}
-			}
+		//Check if we are in the designer
+		var isDesignerRun = false;
+		console.log(this.id, this.xcpId);
+		if(this.id == this.xcpId) {
+			isDesignerRun = true;
 		}
+		console.log('isDesignerRun', isDesignerRun);
 		
-		//xCP designer only likes asynch calls with TRUE; FALSE will make the page hang in designer; Might be a firewall thing
-		xhr.open("GET", urlCurrentUser, true);
-		xhr.send();
-		flag = 1;
-		//xCP designer only likes asynch calls with TRUE; FALSE will make the page hang in designer; Might be a firewall thing
-		xhr.open("GET", urlRepositoryName, true);
-		xhr.send();
-
-		console.log(dataUser);
-		console.log(dataRepo);
-		
-		var urlValueOld = urlValue;
-		urlValue += '?tagId='+tagId+'&user='+dataUser+'&repository='+dataRepo+'&ticket=admin';
-		console.log(urlValue);
-		
-		var oldRawValue = this.getRawValue();
-
-        //Apply formatting if configured.
-        xcpmi.widgets.form.ValueDisplay.superclass.setValue.apply(this, arguments);
-        value = xcp.formatter.Util.formatWidgetValues(this, value,
-                            xcp.Strings.widget.form.ValueDisplay.trueStr,
-                            xcp.Strings.widget.form.ValueDisplay.falseStr);
-
-        //Set the new URL value as value for the widget
-		if(oldRawValue == '' || urlValueOld == undefined) {
-			this.setRawValue(value);
+		if(isDesignerRun) {
+			this.callParent(arguments);
 		} else {
-			this.setRawValue('<IFRAME style="height:300px; width:400px;" src="'+urlValue+'"></IFRAME>');
+			//Javascript in MyInsight report
+	    	//parent.Ext.getCmp(tagId).fireEvent('mi_event', 'test');
+			
+			//get id from widget
+			var tagId = this.id;
+			console.log(tagId);
+			
+			//get prefix myInsight URL
+			//http://{host_name}:{port}/eDRG
+			var urlValue = this.url;
+			console.log(urlValue);
+			
+			//Rewrite URL with correct paramaters; they should come from a server call
+			//http://{host_name}:{port}/eDRG?clsValue=MI&user=dmadmin&repository=MY_REPO&ticket=admin
+			
+			/*
+			//Does not support async calls!
+			Ext.Ajax.request({
+				async: false,
+				url: 'http://172.25.180.131:8081/MyInsight/currentuser',
+				callback: function(options, success, response) {
+					console.log(JSON.parse(response.responseText).properties.login_name);
+				}
+			});
+			*/
+			
+			var xhr = new XMLHttpRequest();
+			var dataUser = undefined;
+			var dataRepo = undefined;
+			var flag = 0;
+			var urlCurrentUser = 'http://172.25.180.131:8081/MyInsight/currentuser';
+			var urlRepositoryName = 'http://172.25.180.131:8081/MyInsight/repositories/xcp_repository';
+			xhr.onreadystatechange = function() {
+				if(xhr.readyState == 4 && xhr.status == 200) {
+					if(flag == 0) {
+						dataUser = JSON.parse(xhr.responseText).properties.login_name;
+						console.log(dataUser);
+					}
+					else {
+						dataRepo = JSON.parse(xhr.responseText).properties.name;
+						console.log(dataRepo);
+					}
+				}
+			}
+			
+			//xCP designer only likes asynch calls with TRUE; FALSE will make the page hang in designer; Might be a firewall thing
+			xhr.open("GET", urlCurrentUser, false);
+			xhr.send();
+			flag = 1;
+			//xCP designer only likes asynch calls with TRUE; FALSE will make the page hang in designer; Might be a firewall thing
+			xhr.open("GET", urlRepositoryName, false);
+			xhr.send();
+	
+			console.log(dataUser);
+			console.log(dataRepo);
+			
+			var urlValueOld = urlValue;
+			urlValue += '?tagId='+tagId+'&user='+dataUser+'&repository='+dataRepo+'&ticket=admin';
+			console.log(urlValue);
+			
+			var oldRawValue = this.getRawValue();
+	
+	        //Apply formatting if configured.
+	        xcpmi.widgets.form.ValueDisplay.superclass.setValue.apply(this, arguments);
+	        value = xcp.formatter.Util.formatWidgetValues(this, value,
+	                            xcp.Strings.widget.form.ValueDisplay.trueStr,
+	                            xcp.Strings.widget.form.ValueDisplay.falseStr);
+	
+	        //Set the new URL value as value for the widget
+			if(oldRawValue == '' || urlValueOld == undefined) {
+				this.setRawValue(value);
+			} else {
+				this.setRawValue('<IFRAME style="height:300px; width:400px;" src="'+urlValue+'"></IFRAME>');
+			}
+	
+	        if (this.rendered && !this.isDestroyed && !this.width && this.getRawValue() != oldRawValue) {
+	            this.updateLayout();
+	        }
 		}
-
-        if (this.rendered && !this.isDestroyed && !this.width && this.getRawValue() != oldRawValue) {
-            this.updateLayout();
-        }
     }
 });
